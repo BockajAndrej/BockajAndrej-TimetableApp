79f3374b8678e11baff1acce274ebf6ff874c5b6
Andrej Bočkaj <106064818+BockajAndrej@users.noreply.github.com>
2025-06-30 23:24:06 +0200

    fix(App): Added app.xaml.cs and app.csproj

---
+++ application/application.App/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using application.App.Pages.View;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace application.App
+{
+    public partial class App : Application
+    {
+        private IServiceProvider _serviceProvider;
+        public App(IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+            _serviceProvider = serviceProvider;
+            //MainPage = serviceProvider.GetRequiredService<AppShell>();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            //return new Window(new AppShell());
+            return new Window(_serviceProvider.GetRequiredService<MainPageView>());
+        }
+    }
+}
